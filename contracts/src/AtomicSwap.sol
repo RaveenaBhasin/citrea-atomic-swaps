7d4ffa6151a2b61fba322dec638a20629a0cb920
byteZorvin <imehul24@gmail.com>
2024-12-07 19:31:11 +0530

    atomic contract done

--- contracts/src/AtomicSwap.sol
+++ contracts/src/AtomicSwap.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.13;
 
 import "bitcoin-spv/solidity/contracts/ValidateSPV.sol";
 import "./interfaces/IBitcoinLightClient.sol";
+// import {IERC20} from "openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {IAtomicSwap, Request, Status} from "./interfaces/IAtomicSwap.sol";
 
 contract AtomicSwap is IAtomicSwap {
@@ -11,13 +12,15 @@ contract AtomicSwap is IAtomicSwap {
     mapping(uint256 => Request) public lockedFunds;
     // id => request
     mapping(uint256 => Request) public requests;
+
     uint256 totalRequests;
 
-    constructor(address _bitcoinLightClient, address _token) {
+    constructor(address _bitcoinLightClient) {
         bitcoinLightClient = IBitcoinLightClient(_bitcoinLightClient);
     }
 
-    function generateRequest(uint256 amount) external {
+    function generateRequest(uint256 amount) external payable {
+        require(msg.value == amount, "Not enough funds sent");
         totalRequests++;
         requests[totalRequests] = Request({
             requestor: msg.sender,
@@ -27,9 +30,51 @@ contract AtomicSwap is IAtomicSwap {
         });
     }
 
-    function getRequest(
-        uint256 _id
-    ) external view returns (Request memory req_) {
+    function getRequest(uint256 _id) public view returns (Request memory req_) {
         return requests[_id];
+    }
+
+    function fullfill(
+        uint256 _requestId,
+        uint256 _blockNumber,
+        bytes32 _wtxId,
+        bytes calldata _proof,
+        uint256 _index
+    ) external {
+        bool is_included = bitcoinLightClient.verifyInclusion(
+            _blockNumber,
+            _wtxId,
+            _proof,
+            _index
+        );
+
+        require(is_included, "Transaction not included");
+        releaseFundsTo(_requestId, msg.sender);
+    }
+
+    function releaseFundsTo(uint256 _requestId, address user) private {
+        Request memory request = getRequest(_requestId);
+
+        // Update status of the request
+        request.status = Status.Fulfilled;
+        requests[_requestId] = request;
+
+        uint256 amount = request.amount;
+        (bool success, bytes memory _data) = user.call{value: amount}("");
+        require(success, "Failed to send funds");
+    }
+
+    function revokeRequest(uint256 _requestId) external {
+        Request memory request = getRequest(_requestId);
+        require(request.requestor == msg.sender, "Un athorised caller");
+
+        // Update status to Revoked
+        request.status = Status.Revoked;
+        requests[_requestId] = request;
+
+        (bool success, bytes memory _data) = request.requestor.call{
+            value: request.amount
+        }("");
+        require(success, "Failed to send funds");
     }
 }
