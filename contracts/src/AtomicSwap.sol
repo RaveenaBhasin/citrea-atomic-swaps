a2f502b508c4062c1cbb0ae6eeb902d20146bbec
RaveenaBhasin <raveenabhasin15@gmail.com>
2024-12-07 13:36:30 +0530

    fix: add bitcoin-spy module

--- contracts/src/AtomicSwap.sol
+++ contracts/src/AtomicSwap.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import "lib/bitcoin-spv/solidity/contracts/ValidateSPV.sol";
+import "bitcoin-spv/solidity/contracts/ValidateSPV.sol";
 import "./IBitcoinLightClient.sol";
 
 contract AtomicSwap {
