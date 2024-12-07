4c8076d447078b20343725cc202d1285cdb49384
byteZorvin <imehul24@gmail.com>
2024-12-07 13:19:22 +0530

    installed dep

---
+++ contracts/src/AtomicSwap.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "lib/bitcoin-spv/solidity/contracts/ValidateSPV.sol";
+import "./IBitcoinLightClient.sol";
+
+contract AtomicSwap {
+    function verifyTxn(uint256 blockNumber) public {}
+}
